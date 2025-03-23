6fc8a472fae88a3be325247e86ae9cef60d88330
David <david.car@gmx.de>
2025-03-23 20:27:47 +0100

    Add German translation for the new options

--- BeamMeUp/localization/DE.lua
+++ BeamMeUp/localization/DE.lua
@@ -177,6 +177,15 @@ mkstr(SI.TELE_SETTINGS_VIEWED_ZONE_ALWAYS_TOP, "Angezeigte Zone & Unterzonen imm
 mkstr(SI.TELE_SETTINGS_VIEWED_ZONE_ALWAYS_TOP_TOOLTIP, "Aktuell angezeigte Zone und Unterzonen (geöffnete Karte) immer an oberster Stelle der Liste anzeigen.")
 mkstr(SI.TELE_SETTINGS_DEFAULT_TAB, "Standardliste")
 mkstr(SI.TELE_SETTINGS_DEFAULT_TAB_TOOLTIP, "Die Liste, die standardmäßig beim Öffnen von BeamMeUp angezeigt wird.")
+mkstr(SI.TELE_SETTINGS_HEADER_CHAT_OUTPUT, "Chat-Meldungen")
+mkstr(SI.TELE_SETTINGS_OUTPUT_FAST_TRAVEL, "Schnellreisevorgänge")
+mkstr(SI.TELE_SETTINGS_OUTPUT_FAST_TRAVEL_TOOLTIP, "Informative Chat-Meldungen über die eingeleiteten Schnellreisen. Fehlermeldungen werden weiterhin im Chat angezeigt.")
+mkstr(SI.TELE_SETTINGS_OUTPUT_ADDITIONAL, "Unterstützende Meldungen")
+mkstr(SI.TELE_SETTINGS_OUTPUT_ADDITIONAL_TOOLTIP, "Weitere hilfreiche Chat-Meldungen zu verschiedenen Aktionen des Addons.")
+mkstr(SI.TELE_SETTINGS_OUTPUT_UNLOCK, "Ergebnisse der Wegschrein-Freischaltung")
+mkstr(SI.TELE_SETTINGS_OUTPUT_UNLOCK_TOOLTIP, "Zwischenergebnisse (entdeckte Wegschreine und XP) und unterstützende Chat-Meldungen der automatischen Wegschrein-Freischaltung.")
+mkstr(SI.TELE_SETTINGS_OUTPUT_DEBUG, "Debug Meldungen")
+mkstr(SI.TELE_SETTINGS_OUTPUT_DEBUG_TOOLTIP, "Technische Chat-Meldungen zur Fehlerbehebung. Dadurch wird der Chat zugespammt. Bitte nur auf Anfrage und nur für kurze Zeit verwenden!")
 
 
 -----------------------------------------------------------------------------

afd22b6def0550326989da29eb8bf5c96c4b8b3f
Sara Jarjoura <sara@sensibleweather.com>
2026-01-21 00:20:42 -0500

    fix(console) Kind of working?

--- BeamMeUp/Console/BaseGlobals.lua
+++ BeamMeUp/Console/BaseGlobals.lua
@@ -7,15 +7,15 @@ BG.LHAS = LibHarvensAddonSettings
 BG.ZoneStoryFragment = WORLD_MAP_ZONE_STORY_GAMEPAD_FRAGMENT
 BG.ZO_WorldMapZoneStoryTopLevel = ZO_WorldMapZoneStoryTopLevel_Gamepad
 BG.AntiquityLore = ANTIQUITY_LORE_GAMEPAD
-BG.ZoFontGame = ZoFontGamepad22
+BG.ZoFontGame = ZoFontGamepad36
 BG.ZoFontGameBookTable = ZoFontGamepadBookTablet
 BG.CollectionsBook = GAMEPAD_COLLECTIONS_BOOK
 
 -- Settings vars
 local function zipDropdownSortedKeys(choices)
   local dropdownSortItems = {}
-  for k, v in pairs(choices) do
-    table.insert(dropdownSortItems, { name = v.name, data = k } )
+  for k, v in ipairs(choices) do
+    table.insert(dropdownSortItems, { name = v, data = k } )
   end
   return dropdownSortItems
 end
@@ -23,7 +23,7 @@ end
 local function zipDropdownSortedValues(choices, values)
   local dropdownSortItems = {}
   for k, v in pairs(choices) do
-    table.insert(dropdownSortItems, { name = v.name, data = values[k] } )
+    table.insert(dropdownSortItems, { name = v, data = values[k] } )
   end
   return dropdownSortItems
 end
